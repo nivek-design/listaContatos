e09a04532a7538ddc3b9d46568e06a5f3af5fc4d
Kevin Araujo <nivekdesign@outlook.com>
2021-11-19 19:53:04 -0300

    criado pagina de info do contato

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { TodosContatos } from './../models/TodosContatos.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -11,7 +12,42 @@ import { ToastController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(public alertController: AlertController,public toastController: ToastController) {}
+  contato = 'Contatos';
+
+  listaContatos: TodosContatos[]=[
+    {
+      nome: 'Kevin',
+      sobrenome: 'Araujo',
+      celular: 43984021307,
+      // eslint-disable-next-line max-len
+      foto: 'https://scontent.fumu2-2.fna.fbcdn.net/v/t1.6435-9/49274263_2106440106108826_2590806147908763648_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFXiqliyTvXZ2I1z3CEEL-YMDFlaKe0It4wMWVop7Qi3liLOYI-B0UekWaOsCfxq4kGvXRQnbxYpn3MdXFL40Sl&_nc_ohc=v6AyKtVK2gcAX9m--nK&_nc_ht=scontent.fumu2-2.fna&oh=da60eaa1f57dbc2a21ad87bf1f590fcc&oe=61BA7A6B',
+      informacoes: 'moro em londrina paraná ',
+      email: 'nivekdesign@outlook.com',
+      pagina: '/informacoes-contato'
+
+    },
+    {
+      nome: 'Laura',
+      sobrenome: 'Rossini',
+      celular: 43998726322,
+      foto: '',
+      informacoes: '?',
+      email: '',
+      pagina: '/informacoes-contato',
+      pagina2:'/novo-contato'
+
+    }
+
+
+    ];
+
+
+
+
+
+
+
+  constructor(public alertController: AlertController,public toastController: ToastController,) {}
 
   async deleteContato() {
     const alert = await this.alertController.create({
@@ -40,8 +76,8 @@ export class HomePage {
     const toast = await this.toastController.create({
       message: 'Deletado com sucesso!',
       duration: 2000,
-      color: 'danger',
-      position: "top"
+      color: 'verdefluorescente',
+      position: 'bottom',
     });
     toast.present();
   }
